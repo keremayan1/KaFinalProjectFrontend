925781473f508f49da41b52cebf8b436ef6a6377
Kerem Ayan <keremayan002@gmail.com>
2021-08-16 15:33:46 +0300

    added2

--- kafinalprojectfrontend/src/app/components/category/category.component.ts
+++ kafinalprojectfrontend/src/app/components/category/category.component.ts
@@ -9,14 +9,25 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
+  currentCategory: Category;
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    this.getCategories()
+    this.getCategories();
   }
   getCategories() {
     this.categoryService
       .getCategories()
       .subscribe((response) => (this.categories = response.data));
+  }
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
   }
 }
